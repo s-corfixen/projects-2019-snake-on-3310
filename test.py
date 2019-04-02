91dcf6c4a4e0e85f60af713c45cd15ad07d1fc83
s-corfixen <brq913@alumni.ku.dk>
2019-04-02 18:03:30 +0200

    yes

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def ok():
     del headers[-1]
     dict1 = dict.fromkeys(headers, ["*"])
     dataset = Dst.get_data(table_id=id, variables = dict1)
-    dataset.to_excel('projects-2019-snake-on-3310/data.xlsx',  sheet_name='Sheet1')
+    dataset.to_excel('c:/Users/Corfixen/Documents/projects-2019-snake-on-3310/data.xlsx',  sheet_name='Sheet1')
     
 button = tk.Button(master, text="Load", command = ok)
 button.pack()
